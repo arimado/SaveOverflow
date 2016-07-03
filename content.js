c186bd3e8bc3e7597fe388d13e7a0b2871000d0e
Jeremy Arimado <jarimado@gmail.com>
2016-07-03 22:53:22 +1000

    added click and message listener = logging link to console on click:

--- content.js
+++ content.js
@@ -1,2 +1,10 @@
-var firstHref = $("a[href^='http']").eq(0).attr("href");
-console.log(firstHref);
+// content.js
+chrome.runtime.onMessage.addListener(
+  function(request, sender, sendResponse) {
+    if( request.message === "clicked_browser_action" ) {
+      var firstHref = $("a[href^='http']").eq(0).attr("href");
+
+      console.log(firstHref);
+    }
+  }
+);
